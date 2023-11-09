e1db231f2b50ae7cb4167368d7f484114f4d2e31
Colin Wee <2gbleh@gmail.com>
2023-11-08 23:26:18 -0800

    2022 day02 part2

--- 2022/02/solve/src/main.rs
+++ 2022/02/solve/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::BufRead;
 
 
-fn get_score(their_move: &str, your_move: &str) -> i32
+fn get_score_part1(their_move: &str, your_move: &str) -> i32
 {
     return if their_move == "A" {
         match your_move {
@@ -29,6 +29,7 @@ fn get_score(their_move: &str, your_move: &str) -> i32
     }
 }
 
+
 fn part1() -> anyhow::Result<()>
 {
     // A for Rock, B for Paper, and C for Scissors
@@ -44,7 +45,62 @@ fn part1() -> anyhow::Result<()>
         let their_move = t.next().unwrap();
         let your_move = t.next().unwrap();
 
-        let score = get_score(their_move, your_move);
+        let score = get_score_part1(their_move, your_move);
+
+        println!("{their_move} {your_move} {score}");
+        total_score += score;
+    }
+
+    println!("{total_score}");
+    return Ok(());
+}
+
+
+fn get_score_part2(their_move: &str, your_move: &str) -> i32
+{
+    return if their_move == "A" {
+        match your_move {
+            "X" => 0 + 3,
+            "Y" => 3 + 1,
+            "Z" => 6 + 2,
+            _ => panic!("oo"),
+        }
+    } else if their_move == "B" {
+        match your_move {
+            "X" => 0 + 1,
+            "Y" => 3 + 2,
+            "Z" => 6 + 3,
+            _ => panic!("oo"),
+        }
+    } else if their_move == "C" {
+        match your_move {
+            "X" => 0 + 2,
+            "Y" => 3 + 3,
+            "Z" => 6 + 1,
+            _ => panic!("oo"),
+        }
+    } else {
+        panic!("ee");
+    }
+}
+
+
+fn part2() -> anyhow::Result<()>
+{
+    // A for Rock, B for Paper, and C for Scissors
+    // X means lose, Y means end the round in a draw, and Z means win
+
+    let mut total_score = 0;
+
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        
+        let mut t = line.split(" ");
+        let their_move = t.next().unwrap();
+        let your_move = t.next().unwrap();
+
+        let score = get_score_part2(their_move, your_move);
 
         println!("{} {} {}", their_move, your_move, score);
         total_score += score;
@@ -54,7 +110,8 @@ fn part1() -> anyhow::Result<()>
     return Ok(());
 }
 
+
 fn main() -> anyhow::Result<()>
 {
-    return part1();
+    return part2();
 }
