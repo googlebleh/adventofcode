8e32b07ef528756b22064d1b7296747c978783e9
Colin Wee <2gbleh@gmail.com>
2023-11-08 22:22:04 -0800

    2022 day01 part2

--- 2022/01/solve/src/main.rs
+++ 2022/01/solve/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::BufRead;
 
-fn main() -> anyhow::Result<()>
+fn part1() -> anyhow::Result<()>
 {
     let mut max_load = -1;
     let mut curr_load = 0;
@@ -28,4 +28,39 @@ fn main() -> anyhow::Result<()>
     }
 
     return Ok(());
+}
+
+fn part2() -> anyhow::Result<()>
+{
+    let mut all_loads = std::vec::Vec::new();
+    let mut curr_load = 0;
+
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line?;
+
+        if line == "" {
+            all_loads.push(curr_load);
+            curr_load = 0;
+
+        } else {
+            let calories = line.parse::<i32>()?;
+            curr_load += calories;
+        }
+    }
+    all_loads.push(curr_load);
+
+    all_loads.sort();
+    all_loads.reverse();
+    let top3 = &all_loads[..3];
+    println!("{:?}", top3);
+    println!("{:?}", top3.iter().sum::<i32>());
+
+    return Ok(());
+}
+
+fn main() -> anyhow::Result<()>
+{
+    // return part1();
+    return part2();
 }
