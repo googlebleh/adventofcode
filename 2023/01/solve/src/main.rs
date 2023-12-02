918b429c9b61c19aa77abc8735960e659f4c185f
Colin Wee <2gbleh@gmail.com>
2023-12-01 17:31:39 -0800

    2023 day01 part2

--- 2023/01/solve/src/main.rs
+++ 2023/01/solve/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::io::BufRead;
 
 fn get_first_char(s: impl Iterator<Item = char>) -> anyhow::Result<u32>
@@ -25,7 +27,58 @@ fn part1() -> anyhow::Result<()>
     return Ok(());
 }
 
+fn parse_digit(s: &str, i: usize) -> Option<u32>
+{
+    if let Some(d) = s.chars().nth(i).unwrap().to_digit(10) {
+        return Some(d);
+    }
+
+    let lut = [
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+    for (name, val) in lut {
+        if s[i..].starts_with(name) {
+            return Some(val);
+        }
+    }
+    return None;
+}
+
+fn find_digit(s: &str, it: impl Iterator<Item = usize>) -> anyhow::Result<u32>
+{
+    for i in it {
+        if let Some(d) = parse_digit(&s, i) {
+            return Ok(d);
+        }
+    }
+    anyhow::bail!("no digit!");
+}
+
+fn part2() -> anyhow::Result<()>
+{
+    let stdin = std::io::stdin();
+    let mut cumulative_sum = 0;
+    for line in stdin.lock().lines() {
+        let line = line?;
+
+        let first = find_digit(&line, 0..line.len())?;
+        let last = find_digit(&line, (0..line.len()).rev())?;
+        cumulative_sum += (first * 10) + last;
+    }
+    println!("{cumulative_sum}");
+    return Ok(());
+}
+
 fn main() -> anyhow::Result<()>
 {
-    return part1();
+    return part2();
 }
