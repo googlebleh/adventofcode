91ed0e2734baecfaf75af76456cfd897e7aec54a
Colin Wee <2gbleh@gmail.com>
2023-12-04 11:12:18 -0800

    getinput: argument defaults, silence curl

--- getinput.sh
+++ getinput.sh
@@ -11,19 +11,8 @@ print_usage ()
 	echo "usage: $0 <day number> <year>"
 }
 
-if [ $# -lt 1 ]; then
-	echo $#
-	print_usage
-	exit 2
-fi
-day="$1"
-year="${2:-2023}"
-
-# get_day_number ()
-# {
-# 	bc -l <<< "((`date +%s` - `date -d 'dec 1' +%s`) / (24*3600)) + 1"
-# 		| LC_ALL=C xargs printf "%.*f\n" "$p"
-# }
+day="${1:-$(TZ=America/New_York date +%-d)}"
+year="${2:-$(date +%Y)}"
 
 read -r aoc_session < "$git_rootdir/session_cookie.txt"
-curl -LORJ --cookie "session=$aoc_session" "https://adventofcode.com/$year/day/$1/input"
+curl -sLORJ --cookie "session=$aoc_session" "https://adventofcode.com/$year/day/$1/input"
