9e96ae226661280b792da61aef21eafc8a54d100
Colin Wee <2gbleh@gmail.com>
2023-12-08 21:13:05 -0800

    part1

---
+++ 2023/09/solve.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+def derivative(seq):
+    r = []
+    for i in range(1, len(seq)):
+        r.append(seq[i] - seq[i-1])
+    return r
+
+def get_next_value(seq):
+    if all(e == 0 for e in seq):
+        return 0
+    v = get_next_value(derivative(seq))
+    return seq[-1] + v
+
+total = 0
+for line in fileinput.input():
+    curr = []
+
+    seq = list(map(int, line.split()))
+    total += (get_next_value(seq))
+
+print(total)
+    # while True:
+    #     curr.append(seq)
+    #     next_ = derivative(seq)
+    #     if all(e == 0 for e in next_):
+    #
+    #     seq = next_
