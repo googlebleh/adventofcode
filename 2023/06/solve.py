f6899e13e2bece7edb940f98f98c731333354135
Colin Wee <2gbleh@gmail.com>
2023-12-08 21:37:29 -0800

    part2
    ugly lol

--- 2023/06/solve.py
+++ 2023/06/solve.py
@@ -4,18 +4,24 @@ for line in fileinput.input():
     toks = line.split()
 
     label = toks[0]
-    rest = list(map(int, toks[1:]))
+    rest = int(''.join(toks[1:]))
     if label.startswith("Time"):
         times = rest
     elif label.startswith("Distance"):
         dists = rest
 
-total = 1
-for i, t in enumerate(times):
-    ways_to_win = 0
-    for guess in range(t):
-        if (t - guess) * guess > dists[i]:
-            ways_to_win += 1
-    total *= ways_to_win
+t = times
+dists = [dists]
+i = 0
+for guess in range(t):
+    if (t - guess) * guess > dists[i]:
+        begin = guess
+        break
 
-print(total)
+for guess in range(t-1, -1, -1):
+    if (t - guess) * guess > dists[i]:
+        end = guess
+        break
+
+
+print(end-begin+1)
