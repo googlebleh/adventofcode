fbab51f25bc5e769d6fa883f06625d2a085a60b6
Colin Wee <2gbleh@gmail.com>
2024-12-02 22:40:56 -0800

    part2

--- 2024/02/solve.py
+++ 2024/02/solve.py
@@ -2,16 +2,34 @@
 
 import sys
 
+def safe_pop(levels, i):
+    return (levels[:i] + levels[i+1:])
+
+def inc_or_dec(levels):
+    return ((levels == sorted(levels)) or (list(reversed(levels)) == sorted(levels)))
+
+def is_safe(levels, recurse=True):
+    for i in range(1, len(levels)):
+        delta = abs(levels[i] - levels[i-1])
+        if not(1 <= delta <= 3):
+            if recurse:
+                for i in range(len(levels)):
+                    if is_safe(safe_pop(levels, i), False):
+                        return True
+            return False
+
+    if inc_or_dec(levels):
+        return True
+    elif recurse:
+        for i in range(len(levels)):
+            if is_safe(safe_pop(levels, i), False):
+                return True
+    return False
+
 with open(sys.argv[1]) as f:
     nsafe = 0
     for line in f:
         levels = list(map(int, line.split()))
-        if (levels == sorted(levels)) or (list(reversed(levels)) == sorted(levels)):
-            is_safe = 1
-            for i in range(1, len(levels)):
-                delta = abs(levels[i] - levels[i-1])
-                if not(1 <= delta <= 3):
-                    is_safe = 0
-                    break
-            nsafe += is_safe
+        if is_safe(levels):
+            nsafe += 1
     print(nsafe)
